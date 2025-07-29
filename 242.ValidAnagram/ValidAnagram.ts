8e0dc3c5ab1929d5f15b2d3fb976d711ef1a54f1
LukeZHar <luke.z.harris@hotmail.com>
2025-07-29 16:18:42 +1000

    FeaT: 242. Valid Anagram Typescript

---
+++ 242.ValidAnagram/ValidAnagram.ts
@@ -0,0 +1,16 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+    
+    const charCount: { [key: string]: number } = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        charCount[s[i]] = (charCount[s[i]] || 0) + 1;
+        charCount[t[i]] = (charCount[t[i]] || 0) - 1;
+    }
+    
+    for (const count of Object.values(charCount)) {
+        if (count !== 0) return false;
+    }
+    
+    return true;
+}
