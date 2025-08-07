5f5eae3dafea413d307f4d9381a5aa431bbfc236
LukeZHar <luke.z.harris@hotmail.com>
2025-08-07 13:34:49 +1000

    Feat: 20. Valid Parentheses Typescript

---
+++ 20.ValidParentheses/validParentheses.ts
@@ -0,0 +1,19 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  for (const char of s) {
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) return false;
+      const top = stack.pop();
+      if (
+        (char === ")" && top !== "(") ||
+        (char === "}" && top !== "{") ||
+        (char === "]" && top !== "[")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
