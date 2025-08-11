085b5588181905dfedee6da9db4ed6b5a4bfa8c8
LukeZHar <luke.z.harris@hotmail.com>
2025-08-11 13:03:52 +1000

    Feat: 141. Linked List Cycle Typescript

---
+++ 141.LinkedListCycle/linkedCycle.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    if (!head || !head.next) return false;
+
+    let slow: ListNode | null = head;
+    let fast: ListNode | null = head.next;
+
+    while (fast && fast.next) {
+        if (slow === fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return false;
+}
