adf50b51486edcbf75f97bcbc6cea75ff4daf1a9
LukeZHar <luke.z.harris@hotmail.com>
2025-08-12 17:10:09 +1000

    Feat: 21. Merge Two Sorted Lists Typescript

---
+++ 21.MergeTwoSortedLists/mergeTwoLists.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const dummy = new ListNode(0);
+    let current = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    // If there are remaining nodes in either list, append them
+    if (l1 !== null) {
+        current.next = l1;
+    } else if (l2 !== null) {
+        current.next = l2;
+    }
+
+    return dummy.next;
+}
