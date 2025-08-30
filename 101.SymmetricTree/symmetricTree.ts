1ae0347c17267e7fe2e8f37c667e7a7d946eb1e0
LukeZHar <luke.z.harris@hotmail.com>
2025-08-30 13:39:12 +1000

    FeaT: 101. Symmetric Tree Typescript

---
+++ 101.SymmetricTree/symmetricTree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if (!root) return true;
+    function isMirror(t1: TreeNode | null, t2: TreeNode | null): boolean {
+        if (!t1 && !t2) return true;
+        if (!t1 || !t2) return false;
+        return (t1.val === t2.val)
+            && isMirror(t1.right, t2.left)
+            && isMirror(t1.left, t2.right);
+    }
+    return isMirror(root.left, root.right);
+}
